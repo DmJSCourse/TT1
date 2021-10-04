073b7b67bd045e9dae0d919e5785d9a0f09c4fc1
DmJsCourse <dimonua@gmail.com>
2021-10-04 18:33:33 +0300

    re-worked the solution

--- solution.js
+++ solution.js
@@ -3,21 +3,20 @@ const chooseDistance = (t, k, ls) => {
       return null;
     }
 
-    let bestDistance = 0;
-    let changedList = [...ls];
+    let uniqueRoutes = ls.reduce((last, current) => 
+      last.concat(last.map((k) => k.concat(current))), [[]]);
 
-    for (let iteration = 0; iteration < k; iteration++){
-      let maximumAvailable = Math.max(
-          ...changedList.filter(entry => entry + bestDistance <= t)
-        );
-      
-      if (maximumAvailable >= 0) {
-        changedList.splice(changedList.indexOf(maximumAvailable), 1);
-        bestDistance += maximumAvailable;
-      } else {
-        return null;
-      }
+    let validRoutes = uniqueRoutes.filter(combination => 
+      combination.length === k
+    );
+
+    if (validRoutes.length === 0) {
+      return null;
     }
 
-    return bestDistance;
+    let distances = validRoutes.map(entry => 
+      entry.reduce((acc, curr) => acc + curr)
+    );
+
+    return Math.max(...distances.filter(distance => distance < t))
 };
